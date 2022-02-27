c4afa37eb19beadc21b09e9a36dc690b75c5376a
Keller Lawson <keeperkell@gmail.com>
2022-02-27 00:42:00 -0800

    added more info

--- semantics.cpp
+++ semantics.cpp
@@ -27,7 +27,7 @@ bool stayInScope = true;
 TreeNode *funcScope;
 
 void semanticAnalysis(TreeNode *t){
-    printf("Start of semantic Analysis\n");
+    printf("Start of semantic Analysis: %s\n", t->attr.name);
 
     TreeNode *currentNode;
 
