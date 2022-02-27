4805f23ce434c368bd7b4fbb3bb23d38fabc445d
Keller Lawson <keeperkell@gmail.com>
2022-02-26 23:45:03 -0800

    syntax fix

--- semantics.cpp
+++ semantics.cpp
@@ -32,13 +32,13 @@ void semanticAnalysis(TreeNode *t){
     TreeNode *childNode2 = t->child[1];
     TreeNode *childNode3 = t->child[2];
 
-    print("start of semantic analysis");
+    printf("start of semantic analysis");
     if(!t){
-        print("node is null, returning\n");
+        printf("node is null, returning\n");
         return;
     }
     else{
-        print("node exists, continue checking\n");
+        printf("node exists, continue checking\n");
         switch(t->nodekind){
             
             
