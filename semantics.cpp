08b4316a1f9348dfa6955fcd382e0589fa05e437
Keller Lawson <keeperkell@gmail.com>
2022-02-26 23:59:07 -0800

    removed child decls

--- semantics.cpp
+++ semantics.cpp
@@ -30,26 +30,7 @@ void semanticAnalysis(TreeNode *t){
     printf("start of semantic analysis\n");
 
     TreeNode *currentNode;
-    
-    printf("Start of children decls\n");
-    if(t->child[0] != NULL){
-        printf("childNode1 decl\n");
-        TreeNode *childNode1 = t->child[0];
-    }
-    printf("between children decls\n");
-    if(t->child[1] != NULL){
-        printf("childNode2 decl\n");
-        TreeNode *childNode2 = t->child[1];
-    }
-    /*
-    if(t->child[2]){
-        printf("childNode1 decl\n");
-       TreeNode *childNode3 = t->child[2]; 
-    }
-    */
-    
-    printf("After children decls\n");
-    
+
     if(!t){
         printf("node is null, returning\n");
         return;
