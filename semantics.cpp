9a177338ae030ef73153b596e5606b059c793df4
keeperkell <keeperkell@gmail.com>
2022-03-01 16:02:12 -0800

    fixed segfault in unary/binary

--- semantics.cpp
+++ semantics.cpp
@@ -1,3 +1,4 @@
+
 // // // // // // // // // // // // // // // // // // // // // // // 
 // CS445 - semantics.cpp
 //
@@ -288,7 +289,7 @@ void semanticAnalysis(TreeNode *t){
                         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                         
                         // This print statement is necesary to prevent a segfault I could not figure out
-                        printf("\n");
+                        //printf("\n");
                         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
                         for(int i = 0; i < MAXCHILDREN; i++){
@@ -484,11 +485,14 @@ void unaryBinaryOps(TreeNode *t, ExpKind subkind){
     }
 
     // check right child for Void Call Exp
-    if(rightChildExpType == Void){
-        if(!(rightChild->nodekind == ExpK && rightChild->subkind.exp == CallK)){
+    if(rightChild){
+        if(rightChildExpType == Void){
+            if(!(rightChild->nodekind == ExpK && rightChild->subkind.exp == CallK)){
             rightChildError = true;
+            }
         }
     }
+    
 
     // start error check and print error functions
 
