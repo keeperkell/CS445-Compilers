1aebb28a04bd171f72bbd20f6357171333d72dab
Keller Lawson <keeperkell@gmail.com>
2022-03-08 22:01:28 -0800

    added debug stmts for Opk

--- semantics.cpp
+++ semantics.cpp
@@ -602,6 +602,21 @@ void checkOpK(TreeNode *t){
             else if(!strcmp(t->attr.name, "<") || !strcmp(t->attr.name, ">") || !strcmp(t->attr.name, "=") ||
                     !strcmp(t->attr.name, ">=") || !strcmp(t->attr.name, "<=") || !strcmp(t->attr.name, "!=")){
 
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                printf("Line %d, Comparison Op: '%s'. \n",t->linenum, t->attr.name);
+                if(t->child[0]){
+                    printf("Line %d, Child 0: '%s'. \n", t->linenum, t->child[0]->attr.name);
+                }
+                if(t->child[1]){
+                    printf("Line %d, Child 0: '%s'. \n", t->linenum, t->child[1]->attr.name);
+                }
+
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
                 t->expType = Boolean;
 
                 //check for mathcing types of children
