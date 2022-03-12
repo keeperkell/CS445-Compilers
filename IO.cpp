55cd43b361275521f0d029c479b458ee9cd2c96e
Keller Lawson <keeperkell@gmail.com>
2022-03-12 10:31:58 -0800

    changed output of IO

--- IO.cpp
+++ IO.cpp
@@ -7,27 +7,29 @@
 extern SymbolTable st;
 
 void IOconstructor(){
-    TokenData *tmp;
-    TokenData *tmpChild;
+    TreeNode *tmp;
+    TreeNode *tmpChild;
 
     // 7 IO nodes
     TreeNode *t[7];
 
     // children nodes will all be the same
-    tmpChild->tokeninput = strdup("*dummy*");
+    tmpChild->attr.name = strdup("*dummy*");
     tmpChild->linenum = -1;   
     
     // void output(int)
-    tmp->tokeninput = strdup("output");
-    tmp->linenum = -1;
-    t[0] = newDeclNode(FuncK, tmp);
-    t[0]->child[0] = newDeclNode(ParamK, tmpChild);
+    t[0]->attr.name = strdup("output");
+    t[0]->linenum = -1;
+    t[0]->subkind.decl = FuncK;
+    t[0]->expType = Void;
+    t[0]->child[0] = tmpChild;
     t[0]->child[0]->expType = Integer;
     t[0]->isIO = true;
     st.insert(t[0]->attr.name, t[0]);
 
+    /*
     // void outputb(bool)
-    tmp->tokeninput = strdup("outputb");
+    tmp->attr.name = strdup("outputb");
     tmp->linenum = -1;
     t[1] = newDeclNode(FuncK, tmp);
     t[1]->child[0] = newDeclNode(ParamK, tmpChild);
@@ -36,7 +38,7 @@ void IOconstructor(){
     st.insert(t[1]->attr.name, t[1]);
 
     // void outpuc(char)
-    tmp->tokeninput = strdup("outputc");
+    tmp->attr.name = strdup("outputc");
     tmp->linenum = -1;
     t[2] = newDeclNode(FuncK, tmp);
     t[2]->child[0] = newDeclNode(ParamK, tmpChild);
@@ -45,7 +47,7 @@ void IOconstructor(){
     st.insert(t[2]->attr.name, t[2]);
 
     // int input()
-    tmp->tokeninput = strdup("input");
+    tmp->attr.name = strdup("input");
     tmp->linenum = -1;
     t[3] = newDeclNode(FuncK, tmp);
     t[3]->expType = Integer;
@@ -53,7 +55,7 @@ void IOconstructor(){
     st.insert(t[3]->attr.name, t[3]);
 
     // int inputb()
-    tmp->tokeninput = strdup("inputb");
+    tmp->attr.name = strdup("inputb");
     tmp->linenum = -1;
     t[4] = newDeclNode(FuncK, tmp);
     t[4]->expType = Boolean;
@@ -61,7 +63,7 @@ void IOconstructor(){
     st.insert(t[4]->attr.name, t[4]);
 
     // int inputc()
-    tmp->tokeninput = strdup("inputc");
+    tmp->attr.name = strdup("inputc");
     tmp->linenum = -1;
     t[5] = newDeclNode(FuncK, tmp);
     t[5]->expType = Integer;
@@ -69,11 +71,12 @@ void IOconstructor(){
     st.insert(t[5]->attr.name, t[5]);
 
     // void outnl()
-    tmp->tokeninput = strdup("outnl");
+    tmp->attr.name = strdup("outnl");
     tmp->linenum = -1;
     t[6] = newDeclNode(FuncK, tmp);
     t[6]->child[0] = newDeclNode(ParamK, tmpChild);
     t[6]->child[0]->expType = Void;
     t[6]->isIO = true;
     st.insert(t[6]->attr.name, t[6]);
+    */
 }
