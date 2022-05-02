fb6551c9d337b676fccd323ea5757104e8fda07b
Keller Lawson <keeperkell@gmail.com>
2022-05-01 22:24:45 -0700

    finished Stmt section

--- codegen.cpp
+++ codegen.cpp
@@ -9,6 +9,8 @@ extern SymbolTable st;
 extern int foffset;
 extern int goffset;
 int loffset = -2;
+bool storeInMem = false;
+int breakLoc = 0;
 
 FILE *genFile;
 
@@ -100,12 +102,109 @@ void codeGenDecl(TreeNode *t){
 void codeGenStmt(TreeNode *t){
     switch(t->subkind.stmt){
         case IfK:
+            int thenLineNum = 0;
+            int elseLineNum = 0;
+            storeInMem = false;
+
+            emitComment((char *)("IF"));
+            genParse(t->child[0]);
+            emitComment((char *)("THEN"));
+            thenLineNum = emitSkip(1);
+
+            // second child exists if else is used
+            if(t->child[2]){
+                elseLineNum = emitSkip(1);
+            }
+
+            backPatchAJumpToHere((char *)("JZR"), 3, thenLineNum, (char *)("Jump around THEN if false [backpatch]"));
+
+            if(t->child[2]){
+                emitComment((char *)("ELSE"));
+                genParse(t->child[2]);
+                backPatchAJumpToHere((char *)("JMP"), 3, elseLineNum, (char *)("Jump around ELSE [backpatch]"));
+            }
+            emitComment((char *)("END IF"));
+            break;
+
         case WhileK:
+            int bP = emitSkip(0);
+            int whileJump = 0;
+            int jumpLoc = 0;
+            storeInMem = false;
+
+            emitComment((char *)("WHILE"));
+            genParse(t->child[0]);
+            int bB = breakLoc;
+            breakLoc = emitSkip(0);
+
+            emitRM((char *)"JNZ", 3, 1, 7, (char *)("Jump to WHILE"));
+            emitComment((char *)("DO"));
+
+            whileJump = emitSkip(1);
+            genParse(t->child[1]);
+            emitRM((char *)"JMP", 6, bP - emitSkip(0) - 1, 7, (char *)("Jump to beginning of loop"));
+            
+            jumpLoc = emitSkip(0);
+            backPatchAJumpToHere(whileJump, (char *)("Jump past end of look [backpatch]"));
+            breakLoc = bB;
+            emitNewLoc(jumpLoc);
+            emitComment((char *)("END WHILE"));
+            break;
+            
         case ForK:
+            break;
+
         case CompoundK:
+            int tempOffset = loffset;
+            emitComment((char *)("COMPOUND"));
+            emitComment((char *)("Compound Body"));
+
+            for(int i = 0; i < MAXCHILDREN; i++){
+                genParse(t->child[i]);
+            }
+
+            loffset = tempOffset;
+            emitComment((char *)("END COMPOUND"));
+            break;
+
         case ReturnK:
+            emitComment((char *)("RETURN"));
+			if(t->child[0] != NULL)
+			{
+				if(t->isArray)
+				{
+					if(!strcmp(t->child[0]->attr.name, "["))
+					{
+						
+					}
+					else
+					{
+						storeInMem = false;
+						genParse(t->child[0]);
+						emitRM((char *)"LDA", 2, 0, 3, (char *)("Copy to return register"));
+					}
+					
+				}
+				else
+				{
+					storeInMem = false;
+					genParse(t->child[0]);
+					emitRM((char *)"LDA", 2, 0, 3, (char *)("Copy to return register"));
+				}
+			}
+			emitRM((char *)"LD", 3, -1, 1, (char *)("Load return address"));
+			emitRM((char *)"LD", 1, 0, 1, (char *)("Adjust fp"));
+			emitRM((char *)"JMP", 7, 0, 3, (char *)("Return"));
+            emitComment((char *)("END RETURN"));
+          	break;
+
         case BreakK:
+            emitComment((char *)("BREAK"));
+            emitRM((char *)"JMP", 7, breakLoc - emitSkip(0), 7, (char *)("break"));
+            break;
+
         case RangeK:
+            break;
     }
 }
 
